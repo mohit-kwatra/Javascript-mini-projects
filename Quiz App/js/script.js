b37d9770943737c9d5f395f12fe825b20adf607b
Mohit Kwatra <mohitkwatra118@gmail.com>
2022-04-26 18:02:05 +0530

    Updating Quiz App/js/script.js

--- Quiz App/js/script.js
+++ Quiz App/js/script.js
@@ -1,13 +1,20 @@
-function startQuiz() {
-    if (document.querySelector('.user-name').value == '') 
-    {
-        alert("Please enter your name before starting!");
-    } 
-    else {
-        localStorage.setItem('user',document.querySelector('.user-name').value);
-        location.pathname = '/quiz.html';
-    }
-}
+"use strict";
+window.onload = function() {
 
-let startButton = document.querySelector('.start-button');
-startButton.addEventListener('click', startQuiz);
+    function startQuiz() {
+
+        let userName = document.querySelector('.user-name').value;
+
+        if (userName === '' || userName === null) {
+            alert("Please enter your name before starting quiz!");
+        } else {
+            localStorage.setItem('username', userName);
+            location.assign('./quiz.html');
+        }
+
+    }
+    
+    let startButton = document.querySelector('.start-button');
+    startButton.addEventListener('click', startQuiz);
+
+};
